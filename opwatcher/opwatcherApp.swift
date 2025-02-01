60033399974ca9e2fc35454bdd47055eee4767ca
Michele Lana <36988839+skippydream@users.noreply.github.com>
2025-02-01 03:02:39 +0100

    Initial Commit

---
+++ opwatcher/opwatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  opwatcherApp.swift
+//  opwatcher
+//
+//  Created by Michele Lana on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct opwatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
